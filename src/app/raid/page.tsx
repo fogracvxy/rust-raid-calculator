c3014a16b220459544434d2db7868227ec441f9f
fogracvxy <marn1002000@gmail.com>
2024-04-19 11:55:56 +0200

    Changes to raid UI sulphur cost calculation

--- src/app/raid/page.tsx
+++ src/app/raid/page.tsx
@@ -155,30 +155,16 @@ const DestructionUI = () => {
 
     return { c4, bullets, rockets, satchel };
   };
-  const calculateTotalSulfur = (
-    selectedMethod: keyof Item["destructionOptions"]
-  ) => {
+  const calculateSulfurCost = (method: keyof Item["destructionOptions"]) => {
     let totalSulfur = 0;
-    console.log(selectedMethod);
-    collection.forEach((colItem) => {
-      const itemName = colItem.item.name;
-      const quantity = colItem.quantity;
 
-      // Find the corresponding item in the items array
-      const item = items.find((item) => item.name === itemName);
+    collection.forEach((c) => {
+      const item = items.find((item) => item.name === c.item.name);
 
       if (item) {
-        // Get the quantity of the selected raid method (e.g., c4, rockets, satchel, bullets)
-        const methodQuantity = item.destructionOptions[selectedMethod];
-
-        // Get the sulfur cost per unit for the selected raid method
-        const sulfurCost = getSulfurCost(selectedMethod);
-
-        // Calculate the total sulfur cost for this item
-        const itemSulfurCost = methodQuantity * quantity * sulfurCost;
-
-        // Add to the total sulfur cost
-        totalSulfur += itemSulfurCost;
+        const methodQuantity = item.destructionOptions[method];
+        const sulfurCost = getSulfurCost(method);
+        totalSulfur += methodQuantity * c.quantity * sulfurCost;
       }
     });
 
@@ -205,25 +191,18 @@ const DestructionUI = () => {
         return 0;
     }
   };
+  const sulfurCosts = {
+    c4: calculateSulfurCost("c4"),
+    rockets: calculateSulfurCost("rockets"),
+    satchel: calculateSulfurCost("satchel"),
+    bullets: calculateSulfurCost("bullets"),
+  };
 
+  const sortedOptions = Object.entries(sulfurCosts).sort(
+    ([, costA], [, costB]) => costA - costB
+  );
   return (
     <div className="p-4 py-8">
-      <div>
-        <label className="mr-2" htmlFor="methodSelect">
-          Select Raid Method:
-        </label>
-        <select
-          id="methodSelect"
-          className="text-black"
-          value={selectedMethod}
-          onChange={handleSelectMethod}
-        >
-          <option value="c4">C4</option>
-          <option value="rockets">Rockets</option>
-          <option value="satchel">Satchel</option>
-          <option value="bullets">Bullets</option>
-        </select>
-      </div>
       <div className="flex justify-center my-8 space-x-4">
         <button
           className={`text-sm font-semibold px-2 py-1 rounded ${
@@ -278,7 +257,7 @@ const DestructionUI = () => {
       <div className="grid grid-cols-3 sm:grid-cols-6 lg:grid-cols-10 gap-4 justify-center items-center">
         {activeCategory && renderItemsByCategory(activeCategory)}
       </div>
-      <div className="mt-8">
+      <div className="mt-8 ml-10">
         <h2 className="text-xl font-bold mb-2">Collection</h2>
         {collection.map((c) => (
           <div key={`${c.item.name}`} className="flex items-center mb-2">
@@ -295,13 +274,13 @@ const DestructionUI = () => {
         ))}
       </div>
 
-      <div className="mt-8">
+      <div className="mt-8 ml-10">
         <h2 className="text-xl font-bold mb-2">Required Resources</h2>
         <div className="flex">
           <div className="flex flex-col mr-4">
             <div className="flex items-center mb-2">
               <Image
-                src="/images/explosive.timed.png"
+                src="/images/c4.png"
                 height={40}
                 width={40}
                 alt="Timed Explosive Charge"
@@ -310,7 +289,7 @@ const DestructionUI = () => {
             </div>
             <div className="flex items-center mb-2">
               <Image
-                src="/images/ammo.rifle.explosive.png"
+                src="/images/bullets.png"
                 height={40}
                 width={40}
                 alt="Explosive Ammo"
@@ -319,7 +298,7 @@ const DestructionUI = () => {
             </div>
             <div className="flex items-center mb-2">
               <Image
-                src="/images/ammo.rocket.basic.png"
+                src="/images/rockets.png"
                 height={40}
                 width={40}
                 alt="Rocket"
@@ -328,7 +307,7 @@ const DestructionUI = () => {
             </div>
             <div className="flex items-center">
               <Image
-                src="/images/explosive.satchel.png"
+                src="/images/satchel.png"
                 height={40}
                 width={40}
                 alt="Satchel Charge"
@@ -340,28 +319,59 @@ const DestructionUI = () => {
           {/* Add more columns here if needed */}
         </div>
       </div>
-      <div className="mt-8">
+      <div className="p-4 py-8 ml-6">
+        <h2 className="text-xl font-bold mb-4">
+          Sulfur Costs Sorted (Lowest to Highest)
+        </h2>
         <div className="flex">
-          <div className="flex flex-col">
-            <h2 className="text-xl font-bold mb-2">
-              Total Sulfur Cost ({selectedMethod})
-            </h2>
-            <div className="flex items-center  mb-2 ">
-              <Image
-                src="/images/sulfur.png"
-                height={40}
-                width={40}
-                alt="Sulfur"
-              />
-              <p className="ml-5 mt-2">
-                {calculateTotalSulfur(selectedMethod)}
-              </p>
+          {sortedOptions.map(([method, cost]) => (
+            <div key={method} className="flex items-center mr-4">
+              {/* Sulfur Image with Dim Overlay */}
+              <div style={{ position: "relative" }}>
+                <Image
+                  src={`/images/${method}.png`} // Assuming you have images named like c4.png, rockets.png, etc.
+                  height={50}
+                  width={50}
+                  alt={method.toUpperCase()}
+                />
+                <div
+                  className="overlay"
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    color: "#DDD71B",
+                    fontSize: 14,
+                    fontWeight: "bold",
+                  }}
+                >
+                  <p className="opacity-100">{cost}</p>
+
+                  <div className="flex flex-col w-20 h-20 absolute top-0 left-0">
+                    {" "}
+                    <Image
+                      src="/images/sulfur.png"
+                      height={15}
+                      width={15}
+                      alt="Sulfur"
+                      className="brightness-100"
+                    />
+                  </div>
+                </div>
+              </div>
+              {/* End Sulfur Image with Dim Overlay */}
             </div>
-          </div>
+          ))}
         </div>
       </div>
 
-      <div className="pt-20 flex justify-center lg:flex lg:justify-start ">
+      <div className="pt-10 flex justify-center lg:flex lg:justify-start ml-10 ">
         {" "}
         <button
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
