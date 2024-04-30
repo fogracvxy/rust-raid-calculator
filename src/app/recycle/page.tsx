c32cde036efaec7ecf952e9c44b64b92b9c0ad58
fogracvxy <marn1002000@gmail.com>
2024-04-30 18:11:54 +0200

    Added recycle calculator

--- src/app/recycle/page.tsx
+++ src/app/recycle/page.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { itemsRecycle } from "./items_recyle";
 import Image from "next/image";
-
-type OtherType = Record<string, number> | string | null;
+import { resources } from "./resources";
 
 interface ItemRecycle {
   name: string;
@@ -10,7 +10,6 @@ interface ItemRecycle {
     scrap: number | null;
     metal: number | null;
     highQualityMetal: number | null;
-    other?: OtherType;
     cloth?: number;
     rope?: number;
   };
@@ -18,61 +17,153 @@ interface ItemRecycle {
 }
 
 const Recycle: React.FC = () => {
-  const renderOther = (other: OtherType | undefined): string => {
-    if (typeof other === "object" && other !== null) {
-      return Object.entries(other)
-        .map(([key, value]) => `${key}: ${value}`)
-        .join(", ");
-    }
-    return other ?? "-";
+  const [selectedItems, setSelectedItems] = useState<
+    { name: string; amount: number }[]
+  >([]);
+  const [mode, setMode] = useState<"Safezone" | "Radtown" | "Default">(
+    "Default"
+  );
+
+  const handleIncrement = (itemName: string) => {
+    setSelectedItems((prevItems) => {
+      const existingItem = prevItems.find((item) => item.name === itemName);
+      if (existingItem) {
+        const updatedItems = prevItems.map((item) =>
+          item.name === itemName ? { ...item, amount: item.amount + 1 } : item
+        );
+        return updatedItems;
+      } else {
+        return [...prevItems, { name: itemName, amount: 1 }];
+      }
+    });
   };
 
-  const TableRow = ({ item, index }: { item: ItemRecycle; index: number }) => (
-    <tr className={`bg-gray-${index % 2 === 0 ? "900" : "800"}`}>
-      <td className="px-4 py-2 text-center">
-        <Image
-          src={item.image}
-          alt={item.name}
-          className="w-8 h-8 rounded-full mx-auto"
-          width={32}
-          height={32}
-        />
-      </td>
-      <td className="px-4 py-2 text-center">{item.name}</td>
-      <td className="px-4 py-2 text-center">{item.yield.scrap ?? "-"}</td>
-      <td className="px-4 py-2 text-center">{item.yield.metal ?? "-"}</td>
-      <td className="px-4 py-2 text-center">
-        {item.yield.highQualityMetal ?? "-"}
-      </td>
-      <td className="px-4 py-2 text-center hidden sm:table-cell">
-        {renderOther(item.yield.other)}
-      </td>
-    </tr>
-  );
+  const handleDecrement = (itemName: string) => {
+    setSelectedItems((prevItems) => {
+      const existingItem = prevItems.find((item) => item.name === itemName);
+      if (existingItem) {
+        const updatedItems = prevItems.map((item) =>
+          item.name === itemName && item.amount > 0
+            ? { ...item, amount: item.amount - 1 }
+            : item
+        );
+        return updatedItems.filter((item) => item.amount > 0);
+      } else {
+        return prevItems;
+      }
+    });
+  };
+
+  const resetSelectedItems = () => {
+    setSelectedItems([]);
+  };
+
+  const getTotalYield = () => {
+    let totals: Record<string, number> = {
+      totalScrap: 0,
+      totalMetal: 0,
+      totalCloth: 0,
+      totalHighQualityMetal: 0,
+    };
+
+    selectedItems.forEach((item) => {
+      const selectedItem = itemsRecycle.find((i) => i.name === item.name);
+      if (selectedItem) {
+        totals.totalScrap += (selectedItem.yield.scrap || 0) * item.amount;
+        totals.totalMetal += (selectedItem.yield.metal || 0) * item.amount;
+        totals.totalCloth += (selectedItem.yield.cloth || 0) * item.amount;
+        totals.totalHighQualityMetal +=
+          (selectedItem.yield.highQualityMetal || 0) * item.amount;
+      }
+    });
+
+    return totals;
+  };
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6 text-center">Recycling Table</h1>
-      <div className="overflow-x-auto">
-        <table className="w-full bg-gray-800 text-white rounded-lg overflow-hidden">
-          <thead>
-            <tr>
-              <th className="px-4 py-2 text-center">Icon</th>
-              <th className="px-4 py-2 text-center">Name</th>
-              <th className="px-4 py-2 text-center">Scrap</th>
-              <th className="px-4 py-2 text-center">Metal</th>
-              <th className="px-4 py-2 text-center">High Quality Metal</th>
-              <th className="px-4 py-2 text-center hidden sm:table-cell">
-                Other
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {itemsRecycle.map((item, index) => (
-              <TableRow key={index} item={item} index={index} />
-            ))}
-          </tbody>
-        </table>
+      <h1 className="text-3xl font-bold mb-6 text-center">
+        Recycling Calculator
+      </h1>
+      <div className="flex justify-center mb-4">
+        <select
+          value={mode}
+          onChange={(e) =>
+            setMode(e.target.value as "Safezone" | "Radtown" | "Default")
+          }
+          className="px-4 py-2 border rounded-md text-black"
+        >
+          <option value="Default">Default Values WIP</option>
+          <option value="Safezone">Safezone</option>
+          <option value="Radtown">Radtown</option>
+        </select>
+      </div>
+      <div className="grid grid-cols-3 gap-4 text-sm lg:text-md">
+        {itemsRecycle.map((item: ItemRecycle, index: number) => {
+          const selectedItem = selectedItems.find((i) => i.name === item.name);
+          const amount = selectedItem ? selectedItem.amount : 0;
+
+          return (
+            <div key={index} className="flex flex-col items-center">
+              <Image src={item.image} alt={item.name} width={80} height={80} />
+              <span className="text-center">{item.name}</span>
+              <div className="flex items-center mt-2">
+                <button
+                  onClick={() => handleDecrement(item.name)}
+                  className="bg-gray-500 px-2 py-1 rounded-l"
+                >
+                  -
+                </button>
+                <span className="px-4">{amount}</span>
+                <button
+                  onClick={() => handleIncrement(item.name)}
+                  className="bg-gray-500 px-2 py-1 rounded-r"
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="mt-8">
+        <h2 className="text-xl font-bold mb-4">Selected Items (Yield) </h2>
+        {/* <ul>
+          {selectedItems.map((item, index) => (
+            <li key={index}>
+              {item.name} - Quantity: {item.amount}
+            </li>
+          ))}
+        </ul> */}
+        <button
+          onClick={resetSelectedItems}
+          className="px-4 py-2 bg-red-500 text-white rounded-md mb-4"
+        >
+          Reset Selected Items
+        </button>
+        <div className="flex flex-wrap">
+          {resources.map((resource) => {
+            const totalResource = getTotalYield()[`total${resource.name}`];
+            return (
+              <div
+                key={resource.name}
+                className="flex items-center mr-4 mb-4 relative"
+              >
+                <div className="relative">
+                  <Image
+                    src={resource.image}
+                    height={50}
+                    width={50}
+                    alt={resource.name}
+                  />
+                  <div className="absolute bottom-0 right-0 bg-black bg-opacity-75 text-white text-xs px-1 py-0.5 rounded">
+                    {totalResource || 0}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
---
+++ src/app/recycle/resources.ts
@@ -0,0 +1,18 @@
+export const resources = [
+  {
+    name: "Scrap",
+    image: "/images/resources/scrap.png",
+  },
+  {
+    name: "Metal",
+    image: "/images/resources/metal.fragments.png",
+  },
+  {
+    name: "Cloth",
+    image: "/images/resources/cloth.png",
+  },
+  {
+    name: "HighQualityMetal",
+    image: "/images/resources/metal.refined.png",
+  },
+];
