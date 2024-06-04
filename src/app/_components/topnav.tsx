d4705b3ad6bf687b29d815987553d3db6c6a8944
fogracvxy <marn1002000@gmail.com>
2024-06-04 14:36:25 +0200

    fixes for nav mobile and larger screens

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -4,10 +4,14 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { fetchServerData, ServerData } from "../utils/fetchsServerData";
 
+// Existing imports and setup...
+
 export function TopNav() {
   const [serverData, setServerData] = useState<ServerData | null>(null);
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchServerData();
@@ -29,52 +33,99 @@ export function TopNav() {
 
   const router = useRouter();
   const pathname = usePathname();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
-    <nav className="flex flex-col md:flex-row justify-between items-center border-b p-4 text-base md:text-xl font-semibold">
-      <div className="flex flex-row gap-4 md:flex-row max-w-6xl font-mono items-center justify-center md:justify-start">
-        <Link
-          className={`link ${
-            pathname === "/" ? "border-b-2" : ""
-          } mb-2 md:mb-0 md:mr-5`}
-          href="/"
-        >
-          Home
-        </Link>
-        <Link
-          className={`link ${
-            pathname === "/raid" ? "border-b-2" : ""
-          } mb-2 md:mb-0 md:mr-5`}
-          href="/raid"
-        >
-          Calculator
-        </Link>
-        <Link
-          className={`link ${
-            pathname === "/recycle" ? "border-b-2" : ""
-          } mb-2 md:mb-0 md:mr-5`}
-          href="/recycle"
-        >
-          Recycle
-        </Link>
-        <Link
-          className={`link ${
-            pathname === "/excavator" ? "border-b-2" : ""
-          } mb-2 md:mb-0 md:mr-5`}
-          href="/excavator"
-        >
-          Excavator
-        </Link>
-        <Link className="link mb-2 md:mb-0" href="/battlemetrics">
-          BattleMetrics
-        </Link>
+    <nav className="flex flex-col md:flex-row justify-between items-center border-b p-4 text-base md:text-xl font-semibold relative">
+      <div className="flex justify-between items-center w-full max-w-6xl">
+        <div className="lg:flex hidden gap-4 lg:flex-row items-center justify-start">
+          <Link
+            className={`link ${
+              pathname === "/" ? "border-b-2" : ""
+            } mb-2 md:mb-0 md:mr-5`}
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className={`link ${
+              pathname === "/raid" ? "border-b-2" : ""
+            } mb-2 md:mb-0 md:mr-5`}
+            href="/raid"
+          >
+            Calculator
+          </Link>
+          <Link
+            className={`link ${
+              pathname === "/recycle" ? "border-b-2" : ""
+            } mb-2 md:mb-0 md:mr-5`}
+            href="/recycle"
+          >
+            Recycle
+          </Link>
+          <Link
+            className={`link ${
+              pathname === "/excavator" ? "border-b-2" : ""
+            } mb-2 md:mb-0 md:mr-5`}
+            href="/excavator"
+          >
+            Excavator
+          </Link>
+          <Link
+            className={`link ${
+              pathname === "/battlemetrics" ? "border-b-2" : ""
+            } mb-2 md:mb-0 md:mr-5`}
+            href="/battlemetrics"
+          >
+            BattleMetrics
+          </Link>
+        </div>
+        <div className="lg:hidden">
+          <button
+            className="focus:outline-none"
+            onClick={toggleMenu}
+            aria-label="Toggle Menu"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {isMenuOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
       </div>
-      <div className="flex flex-col items-center md:items-end mt-2 md:mt-0">
+      <div className=" md:flex md:items-center md:justify-end w-full md:w-auto">
         {pathname !== "/battlemetrics" && (
           <>
             {error && <p>{error}</p>}
             {!error && isLoading && <p>Loading...</p>}
             {!error && !isLoading && serverData && (
-              <p className="text-center font-mono text-sm md:text-right">
+              <p className="text-center font-mono text-sm">
                 <span>
                   {serverData.attributes.players} /{" "}
                   {serverData.attributes.maxPlayers} Players Online
@@ -93,6 +144,60 @@ export function TopNav() {
           </>
         )}
       </div>
+      {isMenuOpen && (
+        <div className="lg:hidden absolute top-10 left-0 right-0 bg-black p-4 shadow-md rounded-md">
+          <div className="flex flex-col gap-4">
+            <Link href="/" onClick={closeMenu}>
+              <span
+                className={`link ${
+                  pathname === "/" ? "border-b-2" : ""
+                } mb-2 md:mb-0 md:mr-5`}
+              >
+                {" "}
+                Home
+              </span>
+            </Link>
+            <Link href="/raid" onClick={closeMenu}>
+              <span
+                className={`link ${
+                  pathname === "/raid" ? "border-b-2" : ""
+                } mb-2 md:mb-0 md:mr-5`}
+              >
+                {" "}
+                Calculator
+              </span>
+            </Link>
+            <Link href="/recycle" onClick={closeMenu}>
+              <span
+                className={`link ${
+                  pathname === "/recycle" ? "border-b-2" : ""
+                } mb-2 md:mb-0 md:mr-5`}
+              >
+                Recycle
+              </span>
+            </Link>
+            <Link href="/excavator" onClick={closeMenu}>
+              <span
+                className={`link ${
+                  pathname === "/excavator" ? "border-b-2" : ""
+                } mb-2 md:mb-0 md:mr-5`}
+              >
+                {" "}
+                Excavator
+              </span>
+            </Link>
+            <Link onClick={closeMenu} href="/battlemetrics">
+              <span
+                className={`link ${
+                  pathname === "/battlemetrics" ? "border-b-2" : ""
+                } mb-2 md:mb-0 md:mr-5`}
+              >
+                BattleMetrics
+              </span>
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
