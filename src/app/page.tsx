378d229ef23b3b86da72f500f0757521c715c401
fogracvxy <marn1002000@gmail.com>
2024-06-06 14:01:21 +0200

    added force wipe countdown for every first thursday each month

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,150 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Hourglass } from "react-loader-spinner";
+interface CountdownTime {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
 
 export default function Home() {
   const router = useRouter();
+  const [countDownTime, setCountDownTime] = useState<CountdownTime>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const interval = setInterval(updateCountdown, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const getNextFirstThursday = (): Date => {
+    let now = new Date();
+    let date = new Date(now.getFullYear(), now.getMonth(), 1);
+
+    // Find the first Thursday of the current month
+    while (date.getDay() !== 4) {
+      date.setDate(date.getDate() + 1);
+    }
+
+    // If today's date is past the first Thursday or it is the first Thursday but past 20:00
+    if (now.getDate() === date.getDate() && now.getHours() < 20) {
+      date = now;
+    } else {
+      if (
+        now > date ||
+        (now.getDate() === date.getDate() && now.getHours() >= 20)
+      ) {
+        date.setMonth(date.getMonth() + 1);
+        date.setDate(1);
+        while (date.getDay() !== 4) {
+          date.setDate(date.getDate() + 1);
+        }
+      }
+    }
+
+    date.setHours(20, 0, 0, 0);
+    // Set the target time to 8 PM
+    return date;
+  };
+
+  const updateCountdown = () => {
+    let nextThursday = getNextFirstThursday();
+
+    let now = new Date();
+    let difference = nextThursday.getTime() - now.getTime();
+
+    // Calculate the remaining time
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    setCountDownTime({ days, hours, minutes, seconds });
+
+    // Update circle references
+  };
 
   const handleEnterCalculator = () => {
     router.push("/raid"); // Navigate to '/raid' route
   };
-
+  const isCountdownZero =
+    countDownTime.days === 0 &&
+    countDownTime.hours === 0 &&
+    countDownTime.minutes === 0 &&
+    countDownTime.seconds === 0;
   return (
-    <main className="flex flex-col items-center justify-between p-4 lg:p-24 bg-black text-white min-h-screen">
+    <main className="flex flex-col items-center justify-between p-4 lg:p-24 bg-black text-white ">
+      {isCountdownZero ? (
+        <div className="mb-12">
+          <Hourglass
+            visible={true}
+            height="40"
+            width="40"
+            ariaLabel="hourglass-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            colors={["rgb(220 38 38)", "rgb(239 68 68)"]}
+          />
+        </div>
+      ) : (
+        <>
+          {/* Countdown Section */}
+          <div className="border-b-4 mb-8 text-2xl">NEXT FORCE WIPE</div>
+          <div className="flex items-start justify-center mb-12 w-full gap-6 ">
+            <div className="timer w-16">
+              <div className="bg-red-600 py-4 px-2 rounded-lg overflow-hidden">
+                <h3 className="countdown-element days font-Cormorant font-mono text-2xl text-white text-center">
+                  {countDownTime.days}
+                </h3>
+              </div>
+              <p className="text-lg font-Cormorant font-medium text-white mt-1 text-center w-full">
+                days
+              </p>
+            </div>
+            <div className="timer w-16">
+              <div className="bg-red-600 py-4 px-2 rounded-lg overflow-hidden">
+                <h3 className="countdown-element hours font-Cormorant font-mono text-2xl text-white text-center">
+                  {countDownTime.hours}
+                </h3>
+              </div>
+              <p className="text-lg font-Cormorant font-normal text-white mt-1 text-center w-full">
+                hours
+              </p>
+            </div>
+
+            <div className="timer w-16">
+              <div className="bg-red-600 py-4 px-2 rounded-lg overflow-hidden">
+                <h3 className="countdown-element minutes font-Cormorant font-mono text-2xl text-white text-center">
+                  {countDownTime.minutes}
+                </h3>
+              </div>
+              <p className="text-lg font-Cormorant font-normal text-white mt-1 text-center w-full">
+                minutes
+              </p>
+            </div>
+
+            <div className="timer w-16">
+              <div className="bg-red-600 py-4 px-2 rounded-lg overflow-hidden">
+                <h3 className="countdown-element seconds font-Cormorant font-mono text-2xl text-white text-center">
+                  {countDownTime.seconds}
+                </h3>
+              </div>
+              <p className="text-lg font-Cormorant font-normal text-white mt-1 text-center w-full">
+                seconds
+              </p>
+            </div>
+          </div>
+        </>
+      )}
       <div className="w-full max-w-6xl flex flex-col lg:flex-row items-center justify-between font-mono text-lg">
         {/* Content Section */}
         <div className="w-full lg:w-2/3 lg:order-1 pr-8">
