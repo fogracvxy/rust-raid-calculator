5a834c64ed73f5e6bf9e0a7b04fa5d42f4378bcd
Tristan Chin <tristan.chin@chintristan.io>
2025-06-28 01:40:22 -0400

    Improve image loading on crafting search input

--- src/app/components/search-input.tsx
+++ src/app/components/search-input.tsx
@@ -4,12 +4,16 @@ import type { ClassNames } from "../types/classnames.types";
 import cn from "classnames";
 import Fuse, { type IFuseOptions } from "fuse.js";
 
+export interface SearchRenderOptionProps<T = any> {
+  item: T;
+}
+
 export interface SearchInputProps<T = any> extends Omit<TextInputProps, "className" | "classNames" | "onSelect"> {
   data: T[];
 
   getValue?: (item: T) => string;
   onSelect?: (item: T) => void | { value?: string; focused?: boolean };
-  renderOption?: (params: { item: T }) => React.ReactNode;
+  renderOption?: (params: SearchRenderOptionProps<T>) => React.ReactNode;
   fuzzyOptions?: IFuseOptions<T>;
   inputRef?: LegacyRef<HTMLInputElement>; // forwardRef doesn't work with generics
   className?: string;
@@ -116,31 +120,29 @@ const SearchInput = <T,>({
           }
         }}
       />
-      {isFocused && (
-        <div
-          className={cn(
-            "absolute left-0 right-0 z-10 bg-gradient-to-t from-gray-900 to-black rounded border border-gray-800 shadow-2xl mt-1 max-h-60 overflow-auto",
-            classNames.dropdown
-          )}
-        >
-          {filteredData.length === 0 ? (
-            <div className={cn("p-2 text-gray-500")}>No results found</div>
-          ) : (
-            filteredData.map((item, i) => (
-              <div
-                ref={i === focusedIndex ? focusedIndexRef : null}
-                key={getValue(item)}
-                className={cn("cursor-pointer px-3 py-2 hover:bg-gray-800", classNames.option, {
-                  "bg-gray-700": focusedIndex === i,
-                })}
-                onMouseDown={() => handleSelect(item)}
-              >
-                {renderOption?.({ item }) ?? getValue(item)}
-              </div>
-            ))
-          )}
-        </div>
-      )}
+      <div
+        className={cn(
+          "absolute left-0 right-0 z-10 bg-gradient-to-t from-gray-900 to-black rounded border border-gray-800 shadow-2xl mt-1 max-h-60 overflow-auto",
+          classNames.dropdown,
+          {
+            hidden: !isFocused,
+          }
+        )}
+      >
+        <div className={cn("p-2 text-gray-500", { hidden: filteredData.length > 0 })}>No results found</div>
+        {filteredData.map((item, i) => (
+          <div
+            ref={i === focusedIndex ? focusedIndexRef : null}
+            key={getValue(item)}
+            className={cn("cursor-pointer px-3 py-2 hover:bg-gray-800", classNames.option, {
+              "bg-gray-700": focusedIndex === i,
+            })}
+            onMouseDown={() => handleSelect(item)}
+          >
+            {renderOption?.({ item }) ?? getValue(item)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
